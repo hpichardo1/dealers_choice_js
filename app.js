5498ed5c9f0251917d4f5b6ae2840bb454a9d3d0
heinnssin <heinnssinp@gmail.com>
2021-05-13 21:32:04 -0400

    completed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const PORT = 3000;
+
 app.get("/", (req, res, next) => {
   console.log(req.params)
 res.send( `<!DOCTYPE html>
@@ -12,15 +14,13 @@ res.send( `<!DOCTYPE html>
   <title>Document</title>
 </head>
 <body>
-<h1>PEOPLE</h1>
+<h1>People</h1>
 
 <ul>
 
-<li> <a href='/users/1'> Allen Iverson</a> </li>
-
-<li>  <a href='/users/2'> Alex Rodriguez</a> </li>
-
-<li> <a href='/users/3'> Peyton Manning</a> </li>
+<li> <a href='/users/1'> Allen Iverson  </a> </li>
+<li> <a href='/users/2'> Alex Rodriguez </a> </li>
+<li> <a href='/users/3'> Peyton Manning </a> </li>
 
 </ul>
   
@@ -28,7 +28,83 @@ res.send( `<!DOCTYPE html>
 </html>`)
 });
 
-const PORT = 1337;
+app.get('/users/:id', (req, res, next) =>{
+
+    if (req.params.id === '1'){
+      res.send(
+        `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+      <h1><a href = '/' >Home</a></h1>
+      
+      <ul>
+      <li> BasketBall Player </li>
+      
+      <li> Point Guard </li>
+      
+      <li> Legend </li>
+      </ul>
+        
+      </body>
+      </html>`)
+    } else if (req.params.id === '2'){
+      res.send(
+        `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+      <h1><a href = '/' >Home</a></h1>
+      
+      <ul>
+      <li> Baseball Player </li>
+      
+      <li> Shortstop </li>
+      
+      <li> Yankee </li>
+      </ul>
+        
+      </body>
+      </html>`
+      )
+    } else if (req.params.id === '3'){
+      res.send(
+        `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+      <h1><a href = '/' >Home</a></h1>
+      
+      <ul>
+      <li> Football player </li>
+      
+      <li> Quarterback </li>
+      
+      <li> Hall of Famer </li>
+      </ul>
+        
+      </body>
+      </html>`
+      )
+    }
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
